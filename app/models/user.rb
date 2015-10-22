74aeb3101a52f2c3617a0cac2e58c0abffd7aaf4
Tomoyuki KANO <tkano@conextivo.co.jp>
2015-10-22 19:20:56 +0900

    changed to file backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   mount_uploader :image2, Image2Uploader
   mount_uploader :image3, Image3Uploader
   mount_uploader :image4, Image4Uploader
+
+  attr_accessor :image1_cache, :image1_cache, :image1_cache, :image4_cache
 end
